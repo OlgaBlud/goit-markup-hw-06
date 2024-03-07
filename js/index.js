fb7d54c9bcd6178cc87adaa59365fd013ed99bf9
Olga Blud <olga.blud@gmail.com>
2024-03-07 18:29:07 +0200

    basic structure modal menu with open/close js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,21 @@
     modal: document.querySelector('[data-modal]'),
   };
 
+  refs.openModalBtn.addEventListener('click', toggleModalMenu);
+  refs.closeModalBtn.addEventListener('click', toggleModalMenu);
+
+  function toggleModalMenu() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[menu-modal-open]'),
+    closeModalBtn: document.querySelector('[menu-modal-close]'),
+    modal: document.querySelector('[menu-modal]'),
+  };
+
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
