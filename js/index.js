24260966d2355e9e55b8540b63fc7da6cde71e62
Olga Blud <olga.blud@gmail.com>
2024-03-12 00:39:59 +0200

    add media for team section

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-// subscription
+// modal form
 
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-open');
-//   }
-// })();
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
 
 // modal menu
 
